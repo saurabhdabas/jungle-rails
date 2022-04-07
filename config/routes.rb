64503bc5dac501ee026d15404b00b9e0087d425b
Saurabh Dabas <developerdabas@gmail.com>
2022-04-08 05:03:03 +0530

    Add routes for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
