1546f80082a12ca676bd1e3ee1b2e618df0f1fc3
saurabhdabas <developerdabas@gmail.com>
2022-04-12 00:24:42 +0000

    Validation for empty password

--- spec/models/user_spec_spec.rb
+++ spec/models/user_spec_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe UserSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'password field cannot be empty' do
+      @user = User.new(
+        first_name: "Saurabh",
+        last_name: "Dabas",
+        email: "test@test.com",
+        password: nil,
+        password_confirmation: "@1%2"
+      )
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Password cannot be blank.")
+    end
+
+  end
+
+
 end
