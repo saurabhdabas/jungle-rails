dd69269e08f9de2a8b1740c1d7ff796ba1abed68
saurabhdabas <developerdabas@gmail.com>
2022-04-12 00:01:48 +0000

    Add validation for - Name can't be empty

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,16 +3,30 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'saves a product with a name, price, quantity and category' do
-      @category = Category.new(name: "Furniture")
+      @category = Category.new(name: "Cars")
       @product = Product.new(
-        name: 'Teak Wood Chair',
-        price: 60,
+        name: 'Tesla',
+        price: 60000,
         quantity: 8,
         category: @category
       )
       @product.save
       expect(@product).to be_valid
     end
+
+    it 'validates :name, presence: true' do
+      @category = Category.new(name: "Cars")
+      @product = Product.new(
+        name: nil,
+        price: 5,
+        quantity: 20,
+        category: @category
+      )
+      @product.save
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Name cannot be empty")
+    end
+
   end
 end 
 
