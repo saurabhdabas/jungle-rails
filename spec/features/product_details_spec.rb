1f0d0f5cc7dc69f02244fbf81a49c3e3b851a5b1
saurabhdabas <developerdabas@gmail.com>
2022-04-13 02:59:51 +0000

    Add E2E test for add to cart

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -10,7 +10,6 @@ RSpec.feature "Visitor navigates to a product page", type: :feature, js: true do
       @category.products.create!(
         name: Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
